6ccbe19e21c4f5f741b66db3a4c5593e251af01c
ForeverWyq <987043378@qq.com>
2024-01-22 22:00:59 +0800

    build

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,7 +1,9 @@
 import MysqlDB from './MysqlDB';
 import MysqlPlus from './MysqlPlus';
+import * as CONSTANT from './utils/CONSTANT';
 declare const _default: {
     MysqlDB: typeof MysqlDB;
     MysqlPlus: typeof MysqlPlus;
+    CONSTANT: typeof CONSTANT;
 };
 export default _default;
